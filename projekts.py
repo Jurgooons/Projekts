68642c1dd05534a7940034f5eb184360b7084b06
Jurgooons <50371602+Jurgooons@users.noreply.github.com>
2024-10-01 15:03:21 +0300

    Update projekts.py
    g;s

--- projekts.py
+++ projekts.py
@@ -1,23 +1,29 @@
-import math as m
+from statistics import mode
+
+def Vid(dati): 
+    return sum(dati) / len(dati) 
+
 dati = []
-n = int(input("Ieraksti datu skaitu : "))
+n = int(input("Ieraksti datu skaitu : ",))
 for i in range(0, n):
     ele = int(input())
     dati.append(ele)
+
 dati.sort
-print("Lielākais skaitlis ir", max(dati)-min(dati))
-print("Mazākais skaitlis ir", min(dati))
-print("Amplituda ir", max(dati)-min(dati))
+
 if(len(dati) %2 == 0):
     ind1 = int((len(dati)-1)//2)
     ind2 = int(((len(dati))/2))
     # print(ind1,ind2)
-    summa = dati[ind1]+dati[ind2]
-    print("Moda ir:", summa/2)
+    med = (dati[ind1]+dati[ind2])/2
+    print("Mediāna ir:", med)
     
 else:
     indeks = len(dati)//2
-    print(dati[indeks])
+    print("Mediāna ir :",dati[indeks])
 
-vid = sum(dati)/len(dati)
-print("Vidējais ir", vid)
+print("Lielākais skaitlis ir", max(dati))
+print("Mazākais skaitlis ir", min(dati))
+print("Amplituda ir", max(dati)-min(dati))
+print("Vidējais ir", Vid(dati))
+print("Moda ir", mode(dati))
